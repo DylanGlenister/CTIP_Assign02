641256ec45d17bbf13fff25f535011287ebe801a
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2023-10-26 15:43:17 +1100

    Added phpenhancements.php page

---
+++ phpenhancements.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Mocha - Enhancements</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Dylan Glenister">
+	<meta name="description" content="The php enhancements page for a fictional tech company called Mocha">
+	<meta name="keywords" content="Swinburne, COS10026, assignment, Mocha, php, enhancements">
+	<link rel="stylesheet" href="./styles/styles.css">
+</head>
+<body>
+<?php
+		$title = "PHP Enhancements";
+		include ("header.inc");
+		?>
+	<?php
+		include ("menu.inc");
+		?>
+	<main>
+		<!-- Enhancement 1 -->
+		<section>
+			<h2>Enhancement 1</h2>
+			<p>Information<br>
+		</section>
+		<br>
+		<!-- Enhancement 2 -->
+		<section>
+			<h2>Enhancement 2</h2>
+			<p>Information</p>
+		</section>
+	</main>
+	<?php
+		$author = "Author: Dylan Glenister";
+		include ("footer.inc");
+		?>
+</body>
+</html>
