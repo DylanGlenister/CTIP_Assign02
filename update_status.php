a128b3092fdb01b353f367263478344c3f1b509a
Melusi-N <124911890+Melusi-N@users.noreply.github.com>
2023-10-26 18:20:26 +1100

    Table sort enhancement on manage.php

--- update_status.php
+++ update_status.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="./styles/styles.css">
 
 </head>
 <body>
+    
     <?php
+    $title = "Update EOI Status";
+    include ("header.inc");
     require_once "settings.php";
+    ?>
+    <main>
+    <?php
 
     $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
     if(!$dbconn){
-        echo "<p>Database connection failure.</p>";
+        echo "<h2>Database connection failure.</h2>";
     } else {
         $statusid = $_POST["status$_POST[id]"];
         $query = "UPDATE $sql_table SET status='$statusid' WHERE id='$_POST[id]'";
         $result = mysqli_query($dbconn, $query);
         if (!$result) {
-            echo "<p>There is a problem with" , $query, "</p>";
+            echo "<h2>There is a problem with" , $query, "</h2>";
         } else {
-            echo "<p>Status updated successfully.</p>";
+            echo "<h2>Status updated successfully.</h2>";
         }
         mysqli_close($dbconn);
     }
     ?>
     <p>Return to <a href="./manage.php">manage EOI</a> page.</p>
+    </main>
+    <?php
+		$author = "Melusi Ndebele";
+		include ("footer.inc");
+		?>
 </body>
 </html>
