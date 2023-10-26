602c80b4969ff9d500f213930855963bb2341ab0
Melusi-N <124911890+Melusi-N@users.noreply.github.com>
2023-10-26 15:37:25 +1100

    manage.php fully funtional

--- manage.php
+++ manage.php
@@ -21,13 +21,13 @@
         <input type="checkbox" name="alleoi" id="alleoi">
         <br>
         <label for="refsearch">Job Reference Number:</label>
-        <input type="text" name="refsearch" id="refsearch">
+        <input type="text" name="refsearch" id="refsearch" value="">
         <br>
         <label for="given_namesearch">Given name:</label>
-        <input type="text" name="given_namesearch" id="given_namesearch" >
+        <input type="text" name="given_namesearch" id="given_namesearch" value="">
         <br>
         <label for="surnamesearch">Surname:</label>
-        <input type="text" name="surnamesearch" id="surnamesearch" >
+        <input type="text" name="surnamesearch" id="surnamesearch" value="">
         <br>
         <input type="submit" value="Search">
     </form>
@@ -35,73 +35,101 @@
     <br>
 
     <?php
-        require_once "settings.php";
-        $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
-        if(!$dbconn){
-            echo "<p>Database connection failure.</p>";
-        } else if (isset($_POST["alleoi"])){
-            $query = "SELECT * FROM aplications";
-            $result = mysqli_query($dbconn, $query);
-            if (!$result){
-                echo "<P>There is something wrong with ", $query, "</p>";
-            } else {
-                echo "<table border=\"1\">\n";
+        function display_data($result){
+            echo "<table>\n";
                 echo "<tr>\n"
-                    ."<th scope=\"col\">jobrefnum</th>\n"
+                    ."<th scope=\"col\">Job Reference Number</th>\n"
+                    ."<th scope=\"col\">First Name</th>\n"
+                    ."<th scope=\"col\">Surname</th>\n"
+                    ."<th scope=\"col\">Date of Birth</th>\n"
+                    ."<th scope=\"col\">Gender</th>\n"
+                    ."<th scope=\"col\">Address</th>\n"
+                    ."<th scope=\"col\">Town</th>\n"
+                    ."<th scope=\"col\">State</th>\n"
+                    ."<th scope=\"col\">Postcode</th>\n"
+                    ."<th scope=\"col\">Email</th>\n"
+                    ."<th scope=\"col\">Phone Number</th>\n"
+                    ."<th scope=\"col\">Skills</th>\n"
+                    ."<th scope=\"col\">Other Skills</th>\n"
+                    ."<th scope=\"col\">Status</th>\n"
+                    ."<th scope=\"col\">Update Status</th>\n"
                     ."</tr>\n";
 
+                $ids = array();
+                $i = 0;
                 while ($row = mysqli_fetch_assoc($result)){
                     echo "<tr>\n";
                     echo "<td>",$row["jobrefnum"],"</td>\n";
-                    echo "</tr>\n";
+                    echo "<td>",$row["first_name"],"</td>\n";
+                    echo "<td>",$row["surname"],"</td>\n";
+                    echo "<td>",$row["dob"],"</td>\n";
+                    echo "<td>",$row["gender"],"</td>\n";
+                    echo "<td>",$row["address"],"</td>\n";
+                    echo "<td>",$row["town"],"</td>\n";
+                    echo "<td>",$row["state"],"</td>\n";
+                    echo "<td>",$row["postcode"],"</td>\n";
+                    echo "<td>",$row["email"],"</td>\n";
+                    echo "<td>",$row["phonenum"],"</td>\n";
+                    echo "<td>",$row["skills"],"</td>\n";
+                    echo "<td>",$row["otherskills"],"</td>\n";
+                    echo "<td>",$row["status"],"</td>\n";
+                    echo "<td><form method=\"post\" action=\"update_status.php\">
+                        <input type=\"hidden\" name=\"id\" id=\"id\" value=\"$row[id]\">
+                        <select name=\"status$row[id]\" id=\"status$row[id]\">
+                            <option value=\"not empty\">New</option>
+                            <option value=\"current\">Current</option>
+                            <option value=\"final\">Final</option>
+                        </select>
+                        <input type=\"submit\" value=\"update\">";
+                    echo "</tr></form>\n";
                 }
                 echo "</table>";
                 mysqli_free_result($result);
-            }
-        } else if (isset($_POST["refsearch"])){
-            $query = "SELECT * FROM aplications WHERE jobrefnum = $refsearch";
-            $result = mysqli_query($dbconn, $query);
-            if (!$result){
-                echo "<P>There is something wrong with ", $query, "</p>";
-            } else {
-                echo "<table border=\"1\">\n";
-                echo "<tr>\n"
-                    ."<th scope=\"col\">jobrefnum</th>\n"
-                    ."</tr>\n";
+        }
 
-                while ($row = mysqli_fetch_assoc($result)){
-                    echo "<tr>\n";
-                    echo "<td>",$row["jobrefnum"],"</td>\n";
-                    echo "</tr>\n";
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            require_once ("settings.php");
+
+            $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
+            if(!$dbconn){
+                echo "<p>Database connection failure.</p>";
+            } else if (isset($_POST["alleoi"])){
+                $query = "SELECT * FROM $sql_table";
+                $result = mysqli_query($dbconn, $query);
+                if (!$result){
+                    echo "<P>There is something wrong with ", $query, "</p>";
+                } else {
+                    display_data($result);
                 }
-                echo "</table>";
-                mysqli_free_result($result);
+                mysqli_close($dbconn);
+            } else if ($_POST["refsearch"] != ""){
+                $search = trim($_POST["refsearch"]);
+                $query = "SELECT * FROM $sql_table WHERE jobrefnum = '$search'";
+                $result = mysqli_query($dbconn, $query);
+                if (!$result){
+                    echo "<P>There is something wrong with ", $query, "</p>";
+                } else {
+                    display_data($result);
 
-                echo "<form method=\"post\" action=\"deleteEOI.php\">";
-                echo "<label for=\"refnum\">Delete all EOIs with job reference number:</label>";
-                echo "<input type=\"text\" name=\"refnum\" id=\"refnum\" value=\"<?php echo $refsearch;?>\">";
-                echo "<input type=\"submit\" value=\"Delete\">";
-                echo "</form>";
+                    echo "<form method=\"post\" action=\"deleteEOI.php\">";
+                    echo "<label for=\"refnum\">Delete all EOIs with job reference number: </label>";
+                    echo "<input type=\"text\" name=\"refnum\" id=\"refnum\" value=\"$_POST[refsearch]\">";
+                    echo "<input type=\"submit\" value=\"Delete\">";
+                    echo "</form>";
 
-            }
-        } else if (isset($_POST["namesearch"])){
-            $query = "SELECT * FROM aplications WHERE given_names = $given_namesearch OR surname = $surnamesearch";
-            $result = mysqli_query($dbconn, $query);
-            if (!$result){
-                echo "<P>There is something wrong with ", $query, "</p>";
-            } else {
-                echo "<table border=\"1\">\n";
-                echo "<tr>\n"
-                    ."<th scope=\"col\">jobrefnum</th>\n"
-                    ."</tr>\n";
-
-                while ($row = mysqli_fetch_assoc($result)){
-                    echo "<tr>\n";
-                    echo "<td>",$row["jobrefnum"],"</td>\n";
-                    echo "</tr>\n";
                 }
-                echo "</table>";
-                mysqli_free_result($result);
+                mysqli_close($dbconn);
+            } else if ($_POST["given_namesearch"] != "" or $_POST["surnamesearch"] != ""){
+                $fname = trim($_POST["given_namesearch"]);
+                $sname = trim($_POST["surnamesearch"]);
+                $query = "SELECT * FROM $sql_table WHERE first_name = '$fname' OR surname = '$sname'";
+                $result = mysqli_query($dbconn, $query);
+                if (!$result){
+                    echo "<P>There is something wrong with ", $query, "</p>";
+                } else {
+                    display_data($result);
+                }
+                mysqli_close($dbconn);
             }
         }
 
